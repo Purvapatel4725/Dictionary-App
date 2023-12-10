f7f190811cc062333d13100f9294a4c5b9e42c2b
Purva Patel <144143336+Purvapatel4725@users.noreply.github.com>
2023-12-10 22:36:23 +0000

    Minor fix in about

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,7 +32,7 @@ router.get('/', function(req, res, next) {
 /* GET About page. */
 router.get('/about', function(req, res, next) {
   res.render('partials/about', { 
-    title: 'About Us',
+    title: 'About Me',
     displayName: req.user ? req.user.displayName:''  
   });
 });
